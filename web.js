a57a185830c8a35a01d45f3a1053e736a1784c11
mhazegh <mhazegh@gmail.com>
2013-07-06 18:49:46 -0600

    Changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var res = fs.readFile('/home/micah/bitstarter/index.html','utf8');  
+  response.send(res);
 });
 
 var port = process.env.PORT || 5000;
