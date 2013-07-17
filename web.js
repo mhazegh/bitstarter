e04cad2626dcdb5c44cae48b3d1095679c0ffd1b
mhazegh <mhazegh@gmail.com>
2013-07-16 22:26:28 -0600

    Added path.

--- web.js
+++ web.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 
+app.configure(function(){
+  app.use('/', express.static(__dirname + '/'));
+});
+
 app.get('/', function(request, response) {
   var buf = fs.readFileSync('index.html');
   response.send(buf.toString());
